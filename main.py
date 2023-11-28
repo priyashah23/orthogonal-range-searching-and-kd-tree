3af4f73a2a06d89df5fe61342f8a798c1e6846e5
Priya Shah <priya.shah2309@gmail.com>
2023-11-28 16:29:05 +0000

    Finally got it to work could cry honestly

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import matplotlib.pyplot as plt
 
 
-age = [18, 22, 24, 29, 31, 25, 23, 22, 21]
+age = [18, 22, 24, 29, 31, 25, 23, 20, 21]
 salary = [18_000, 17_500, 22_000, 30_000, 24_000, 19_000, 25_000, 20_000, 19_500]
 
 class KDTreeNode:
@@ -30,20 +30,39 @@ def construct_kd_tree(points: list, depth: int) -> KDTreeNode:
         right_branch = construct_kd_tree(p2, depth+1)
         return KDTreeNode(points[l], left_branch, right_branch)
 
-def search_Kd_tree(root: KDTreeNode, range_list: list, list_of_points):
-    pass
-def region_search(subtree: KDTreeNode, range_list: list, full: bool) -> bool:
-    pass
+def search_Kd_tree(root: KDTreeNode, range_list: list):
+    if root.left is None and root.right is None:
+        if (range_list[0][0] <= root.point[0] <= range_list[0][1]) and (range_list[1][0] <= root.point[1] <= range_list[1][1]):
+            print(root.point)
+    else:
+        if is_fully_contained(root, root.left, range_list):
+            report_subtree(root.left)
+        else:
+            if does_intersect_with_range(root, root.left, range_list):
+                search_Kd_tree(root.left, range_list)
+        if is_fully_contained(root, root.right, range_list):
+            report_subtree(root.right)
+        else:
+            if does_intersect_with_range(root, root.left, range_list):
+                search_Kd_tree(root.right, range_list)
+def is_fully_contained(root:KDTreeNode, subtree: KDTreeNode, range_list: list) -> bool:
+    return ((min(root.point[0], subtree.point[0]) >= range_list[0][0] and max(root.point[0], subtree.point[0]) <= range_list[0][1])
+            and (min(root.point[1], subtree.point[1]) >= range_list[1][0] and max(root.point[1], subtree.point[1]) <= range_list[1][1]))
+def does_intersect_with_range(root:KDTreeNode, subtree: KDTreeNode, range_list: list) -> bool:
+    return ((min(root.point[0], subtree.point[0]) >= range_list[0][0] or max(root.point[0], subtree.point[0]) <=
+             range_list[0][1])
+            or (min(root.point[1], subtree.point[1]) >= range_list[1][0] or max(root.point[1], subtree.point[1]) <=
+                 range_list[1][1]))
 def report_subtree(subtree: KDTreeNode, list_of_points=[]) -> list:
-    pass
+    if subtree.left is None and subtree.right is None:
+        print(subtree.point)
+    else:
+        report_subtree(subtree.left)
+        report_subtree(subtree.right)
 
 if __name__ == '__main__':
     points = list(zip(age, salary))
     kd_tree = construct_kd_tree(points, 0)
     print(kd_tree)
-    range_list = [(23, 29), (20_000, 25_000)]
-    # points_in_range = search_Kd_tree(kd_tree, range_list, [])
-    # print(points_in_range)
-    plt.scatter(age, salary)
-    plt.axvline(24)
-    plt.show()
+    range_list = [(17, 20), (16_000, 20_000)]
+    search_Kd_tree(kd_tree, range_list)
